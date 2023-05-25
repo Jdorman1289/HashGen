785acf0d519f04ccfe996f7f19919646fb9c7ff7
Jesse Dorman <Jdorman1289@gmail.com>
2023-05-25 09:04:06 -0500

    Start writing JS for main functions of the webapp

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let theText = document.getElementById("theText")
+let outputText = document.getElementById("outputText")
+
+function displayHash() {
+    outputText.innerHTML(theText)
+}
