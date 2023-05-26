afd1487a66c9fd824e4e62df9aafa90feba46160
Jesse Dorman <Jdorman1289@gmail.com>
2023-05-26 07:59:19 -0500

    finish basic hash generation function

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 "use strict";
 
-let theText = document.getElementById("theText")
-let outputText = document.getElementById("outputText")
+function generateHashTag() {
+    let outputText = document.getElementById("outputText");
+    let str = document.getElementById("theText").value
+    let list = str.split(" ");
+    let tagList = [];
+    list.forEach(word => {
+        tagList.push(word.charAt(0).toUpperCase() + word.slice(1));
+    });
 
-function displayHash() {
-    outputText.innerHTML(theText)
+    if (tagList.join("") === "") {
+        return false;
+    } else if (tagList.join("").length > 139) {
+        return false;
+    } else {
+        tagList.unshift("#");
+        outputText.innerHTML = (tagList.join(""));
+    }
 }
