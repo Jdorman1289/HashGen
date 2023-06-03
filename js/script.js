a26fc0ece5c1819b1496d09899cb6467e98bffe5
Jesse Dorman <Jdorman1289@gmail.com>
2023-06-03 08:58:44 -0500

    stop trying to merge previous text prediction project

--- js/script.js
+++ js/script.js
@@ -1,125 +1,48 @@
 "use strict";
 
-function generateHashTag() {
-    let outputText = document.getElementById("outputText");
-    let str = document.getElementById("theText").value
-    let list = str.split(" ");
-    let tagList = [];
-    list.forEach(word => {
-        tagList.push(word.charAt(0).toUpperCase() + word.slice(1));
-    });
+// Function to generate hashtags
+function generateHashTags() {
+    // Get user input
+    let inputText = document.getElementById("inputText").value;
+    let outputDiv = document.getElementById("outputDiv");
 
-    if (tagList.join("") === "") {
-        return false;
-    } else if (tagList.join("").length > 139) {
-        return false;
-    } else {
-        tagList.unshift("#");
-        outputText.innerHTML = (tagList.join(""));
-    }
-}
+    // Normalize input text and split it into words
+    let words = inputText.toLowerCase().split(" ");
 
-// Load data file using AJAX
-let data = null;
-let xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-        data = this.responseText;
-    }
-};
-xhttp.open("GET", "shakespeare_set.txt", true);
-xhttp.send();
+    // Initialize an empty array for hashtags
+    let hashtags = [];
 
-// Function to split string into words
-function splitIntoWords(str) {
-    return str.trim().split(/\s+/);
-}
-
-// Total next outcomes function
-function totalNextOutcomes(data, prompt) {
-    let dataWords = splitIntoWords(data);
-    let promptWords = splitIntoWords(prompt);
-    let nextWords = {};
-    let comboSize = promptWords.length;
-    for (let i = 0; i <= dataWords.length - comboSize; i++) {
-        if (dataWords.slice(i, i + comboSize).join(' ') === prompt) {
-            let nextWord = dataWords[i + comboSize];
-            nextWords[nextWord] = (nextWords[nextWord] || 0) + 1;
-        }
-    }
-    return nextWords;
-}
-
-// Most probable word function
-function mostProbableWord(wordCount) {
-    let values = Object.values(wordCount);
-    let isProbabilitySame = values.every(val => val === values[0]);
-    let keys = Object.keys(wordCount);
-    if (isProbabilitySame) {
-        return keys[Math.floor(Math.random() * keys.length)];
-    } else {
-        let highestCount = 0;
-        let mostProbableWord = "";
-        for (let key in wordCount) {
-            if (wordCount[key] > highestCount) {
-                highestCount = wordCount[key];
-                mostProbableWord = key;
-            }
-        }
-        return mostProbableWord;
-    }
-}
-
-let contextWords = [];
-let prompt = window.prompt("Prompt: ");
-let referencePrompt = prompt;
-
-// Loop to generate text continuations
-while (true) {
-    try {
-// Get the dictionary of next words and their count
-        let nextWords = totalNextOutcomes(data, prompt);
-// Get the most probable next word
-        let nextWord = mostProbableWord(nextWords);
-
-        console.clear();
-
-        console.log(referencePrompt + " " + contextWords.join(' '));
-
-        let isKeeping = window.prompt(`Suggested word: '${nextWord}'\n\nPress 1 to keep or 2 to change. `);
-        if (isKeeping === '1') {
-            contextWords.push(nextWord);
-            prompt = contextWords.join(' ');
-        } else {
-            prompt = window.prompt("Enter the next word: ");
-            contextWords.push(prompt);
+    // Loop through the words and generate hashtags
+    for (let word of words) {
+        // Ignore short or meaningless words
+        if (word.length > 2) {
+            // Add the hashtag symbol and push the word to the array
+            hashtags.push("#" + word);
         }
 
-        if (contextWords[contextWords.length - 1].endsWith('.') ||
-            contextWords[contextWords.length - 1].endsWith('?') ||
-            contextWords[contextWords.length - 1].endsWith('!')) {
-            let isDone = window.prompt("\n\nWould you like to continue? Press 1 to continue or 2 to end.\n");
-            if (isDone === '1') {
-                prompt = window.prompt("Prompt: ");
-            } else {
-                break;
-            }
-        }
-    } catch (error) {
-        console.clear();
-        console.log("\n" + referencePrompt + " " + contextWords.join(' '));
-        let quitOnShakespeare = window.prompt("\nShakespeare has no suggestions for that.\nWould you like to continue? Press 1 to continue or 2 to end. ");
-        if (quitOnShakespeare === '1') {
-            prompt = window.prompt("Prompt: ");
-            contextWords.push(prompt);
-        } else {
+        // Limit the number of generated hashtags to 10
+        if (hashtags.length == 10) {
             break;
         }
     }
+
+    // Clear previous hashtags in the output
+    outputDiv.innerHTML = '';
+
+    // Generate clickable hashtags and add them to the output
+    for (let hashtag of hashtags) {
+        // Create new clickable element
+        let newElement = document.createElement('a');
+        newElement.setAttribute('href', 'https://twitter.com/hashtag/' + hashtag.slice(1));
+        newElement.setAttribute('target', '_blank');
+        newElement.textContent = hashtag;
+
+        // Add a space after the hashtag
+        outputDiv.appendChild(newElement);
+        outputDiv.innerHTML += ' ';
+    }
 }
 
-console.clear();
-// Print the generated text
-console.log("\nGenerated Text:\n");
-console.log(referencePrompt + " " + contextWords.join(' '));
-
+// Attach the function to a button
+let generateButton = document.getElementById("generateButton");
+generateButton.addEventListener("click", generateHashTags);
