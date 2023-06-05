87e468d4c27c3d14a276d230827fa50c7c7fb93e
Jesse <Jdorman1289@gmail.com>
2023-06-05 11:07:59 -0500

    revert to state before merging translated code

--- js/script.js
+++ js/script.js
@@ -1,48 +1,20 @@
 "use strict";
 
-// Function to generate hashtags
-function generateHashTags() {
-    // Get user input
-    let inputText = document.getElementById("inputText").value;
-    let outputDiv = document.getElementById("outputDiv");
+function generateHashTag() {
+    let outputText = document.getElementById("outputText");
+    let str = document.getElementById("theText").value
+    let list = str.split(" ");
+    let tagList = [];
+    list.forEach(word => {
+        tagList.push(word.charAt(0).toUpperCase() + word.slice(1));
+    });
 
-    // Normalize input text and split it into words
-    let words = inputText.toLowerCase().split(" ");
-
-    // Initialize an empty array for hashtags
-    let hashtags = [];
-
-    // Loop through the words and generate hashtags
-    for (let word of words) {
-        // Ignore short or meaningless words
-        if (word.length > 2) {
-            // Add the hashtag symbol and push the word to the array
-            hashtags.push("#" + word);
-        }
-
-        // Limit the number of generated hashtags to 10
-        if (hashtags.length == 10) {
-            break;
-        }
-    }
-
-    // Clear previous hashtags in the output
-    outputDiv.innerHTML = '';
-
-    // Generate clickable hashtags and add them to the output
-    for (let hashtag of hashtags) {
-        // Create new clickable element
-        let newElement = document.createElement('a');
-        newElement.setAttribute('href', 'https://twitter.com/hashtag/' + hashtag.slice(1));
-        newElement.setAttribute('target', '_blank');
-        newElement.textContent = hashtag;
-
-        // Add a space after the hashtag
-        outputDiv.appendChild(newElement);
-        outputDiv.innerHTML += ' ';
+    if (tagList.join("") === "") {
+        return false;
+    } else if (tagList.join("").length > 139) {
+        return false;
+    } else {
+        tagList.unshift("#");
+        outputText.innerHTML = (tagList.join(""));
     }
 }
-
-// Attach the function to a button
-let generateButton = document.getElementById("generateButton");
-generateButton.addEventListener("click", generateHashTags);
